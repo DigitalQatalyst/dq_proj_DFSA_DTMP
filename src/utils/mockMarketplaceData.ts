0fefa7599d4b792b51287e61e3c038602a0faf6d
digitalqatalyst1 <dq.demo.a@digitalqatalyst.com>
2025-10-02 12:20:14 +0300

    change card image

--- src/utils/mockMarketplaceData.ts
+++ src/utils/mockMarketplaceData.ts
@@ -255,7 +255,7 @@ export const mockKnowledgeHubItems = [
     },
     tags: ['Export', 'Market Research'],
     imageUrl:
-      'https://images.unsplash.com/photo-1493894473891-10fc1e5dbd22?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80',
+      'https://images.unsplash.com/photo-1554224155-6726b3ff858f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
     date: 'April 18, 2023',
     downloadCount: 1456,
     fileSize: '7.5 MB',
